f3a87f67e6d4f229e1565c2b6e25825c15c84339
Andrius Kvedaras <ikandrius@gmail.com>
2017-08-18 13:52:05 +0300

    Rewrite home tab

--- src/js/playlist/playlist.entries.js
+++ src/js/playlist/playlist.entries.js
@@ -5,7 +5,7 @@ import { getPlaylistById, updatePlaylist } from "./playlist.js";
 import { removePlaylist } from "./playlist.manage.js";
 import { importPlaylist, createImportOptionMask, initGoogleAuth } from "./playlist.import.js";
 
-function createEntryContainer(id) {
+function createContainer(id) {
     const div = document.createElement("div");
     const h3 = document.createElement("h3");
     const ul = document.createElement("ul");
@@ -16,7 +16,8 @@ function createEntryContainer(id) {
 
     h3.textContent = "Playlist entries";
     ul.id = id;
-    ul.addEventListener("click", handleClickOnEntryContainer);
+    ul.addEventListener("click", handleContainerClick);
+    ul.addEventListener("focus", handleContainerFocus, true);
 
     div.appendChild(h3);
     div.appendChild(ul);
@@ -24,15 +25,16 @@ function createEntryContainer(id) {
     return ul;
 }
 
-function removeEntryContainer(container) {
-    container.removeEventListener("click", handleClickOnEntryContainer);
+function removeContainer(container) {
+    container.removeEventListener("click", handleContainerClick);
+    container.removeEventListener("focus", handleContainerFocus, true);
     removeElement(container.parentElement);
 }
 
-function getEntryContainer() {
-    const containerId = "js-pl-entries";
+function getContainer() {
+    const id = "js-pl-entries";
 
-    return getElementById(containerId) || createEntryContainer(containerId);
+    return getElementById(id) || createContainer(id);
 }
 
 function getSyncBtn(id) {
@@ -60,6 +62,20 @@ function disableSyncBtn(id) {
     }
 }
 
+function updatePlaylistStats() {
+    const container = getContainer();
+
+    if (!container) {
+        return;
+    }
+    Array.from(container.children).forEach(entry => {
+        const { tracks, duration } = getPlaylistById(entry.getAttribute("data-entry-id"));
+
+        entry.querySelector(".track-count").textContent = `${tracks.length} tracks`;
+        entry.querySelector(".playlist-duration").textContent = parsePlaylistDuration(duration);
+    });
+}
+
 function getSyncBtnTemplate() {
     return `
         <button class="btn btn-icon pl-entry-btn" data-action="sync" title="Synchronize playlist">
@@ -70,30 +86,49 @@ function getSyncBtnTemplate() {
     `;
 }
 
-function createPlaylistEntry(title, id, url) {
-    const container = getEntryContainer();
-    const btn = url ? getSyncBtnTemplate() : "";
-    const entry = `
-        <li class="pl-entry" data-entry-id=${id}>
-            <form class="pl-entry-form">
-                <input type="text" class="input pl-entry-title" value="${title}" readonly>
-                <button type="submit" class="btn btn-icon pl-entry-btn"
-                    data-action="edit" title="Edit playlist title">
+function getStatusIcon() {
+    return `
+        <svg viewBox="0 0 24 24" class="pl-entry-stats-item">
+            <use href="#lock"></use>
+        </svg>
+    `;
+}
+
+function parsePlaylistDuration(duration) {
+    const hours = Math.floor(duration / 3600);
+    const minutes = Math.ceil(duration / 60 % 60);
+
+    return `${hours} hr ${minutes} min`;
+}
+
+function createPlaylistEntry(pl) {
+    const container = getContainer();
+    const btn = pl.url ? getSyncBtnTemplate() : "";
+    const icon = pl.isPrivate ? getStatusIcon() : "";
+
+    container.insertAdjacentHTML("beforeend", `
+        <li class="pl-entry" data-entry-id=${pl.id}>
+            <div class="pl-entry-input-container" data-action="edit">
+                <input type="text" class="input pl-entry-input" value="${pl.title}">
+                <svg viewBox="0 0 24 24" class="pl-entry-input-icon">
+                    <use href="#edit">
+                </svg>
+            </div>
+            <div class="pl-entry-content">
+                <div class="pl-entry-stats">
+                    ${icon}
+                    <span class="pl-entry-stats-item track-count">${pl.tracks.length} tracks</span>
+                    <span class="pl-entry-stats-item playlist-duration">${parsePlaylistDuration(pl.duration)}</span>
+                </div>
+                ${btn}
+                <button class="btn btn-icon pl-entry-btn" data-action="remove" title="Remove playlist">
                     <svg viewBox="0 0 24 24">
-                        <use href="#edit">
+                        <use href="#trash">
                     </svg>
                 </button>
-            </form>
-            ${btn}
-            <button class="btn btn-icon pl-entry-btn" data-action="remove" title="Remove playlist">
-                <svg viewBox="0 0 24 24">
-                    <use href="#trash">
-                </svg>
-            </button>
+            </div>
         </li>
-    `;
-
-    container.insertAdjacentHTML("beforeend", entry);
+    `);
 }
 
 function removePlaylistEntry(entryElement) {
@@ -102,43 +137,7 @@ function removePlaylistEntry(entryElement) {
     removeElement(entryElement);
 
     if (!parentElement.children.length) {
-        removeEntryContainer(parentElement);
-    }
-}
-
-function updatePlaylistEntryBtn(btn, action) {
-    const nextAction = action === "edit" ? "save": "edit";
-
-    btn.setAttribute("data-action", nextAction);
-    btn.classList.toggle("active");
-}
-
-function editPlaylistTitle(action, parentElement, playlistId) {
-    const titleElement = parentElement.querySelector(".pl-entry-title");
-    const playlistTitle = titleElement.value;
-
-    if (action === "edit") {
-        titleElement.removeAttribute("readonly");
-        titleElement.focus();
-        titleElement.selectionStart = playlistTitle.length;
-    }
-    else if (action === "save") {
-        const { _id, title } = getPlaylistById(playlistId);
-        const newTitle = playlistTitle || title;
-
-        if (newTitle !== title) {
-            editSidebarEntry(playlistId, newTitle);
-            updatePlaylist(playlistId, { title: newTitle });
-            postMessageToWorker({
-                action: "change-title",
-                playlist: {
-                    _id,
-                    title: newTitle
-                }
-            });
-        }
-        titleElement.value = newTitle;
-        titleElement.setAttribute("readonly", "readonly");
+        removeContainer(parentElement);
     }
 }
 
@@ -157,34 +156,72 @@ async function syncPlaylist(id) {
     });
 }
 
-function handleClickOnEntryContainer(event) {
+function editPlaylistTitle({ currentTarget }) {
+    const { attrValue: playlistId } = getElementByAttr("data-entry-id", currentTarget);
+    const { _id, title } = getPlaylistById(playlistId);
+    const newTitle = currentTarget.value;
+
+    currentTarget.removeEventListener("blur", editPlaylistTitle);
+    currentTarget.removeEventListener("keyup", blurEntryInput);
+
+    if (!newTitle) {
+        currentTarget.value = title;
+        return;
+    }
+
+    if (newTitle !== title) {
+        editSidebarEntry(playlistId, newTitle);
+        updatePlaylist(playlistId, { title: newTitle });
+        postMessageToWorker({
+            action: "change-title",
+            playlist: {
+                _id,
+                title: newTitle
+            }
+        });
+    }
+}
+
+function blurEntryInput({ currentTarget, which }) {
+    if (which === 13) {
+        currentTarget.blur();
+    }
+}
+
+function handleContainerClick(event) {
     const entry = getElementByAttr("data-entry-id", event.target);
-    const btn = getElementByAttr("data-action", event.target);
+    const element = getElementByAttr("data-action", event.target);
 
-    event.preventDefault();
-
-    if (!entry || !btn || btn.elementRef.disabled) {
+    if (!entry || !element || element.elementRef.disabled) {
         return;
     }
     const playlistId = entry.attrValue;
-    const action = btn.attrValue;
+    const action = element.attrValue;
 
     if (action === "remove") {
         removePlaylist(playlistId);
         removePlaylistEntry(entry.elementRef);
-        return;
     }
-
-    if (action === "sync") {
+    else if (action === "sync") {
         syncPlaylist(playlistId);
-        return;
     }
-    editPlaylistTitle(action, entry.elementRef,playlistId);
-    updatePlaylistEntryBtn(btn.elementRef, action);
+    else if (action === "edit") {
+        element.elementRef.querySelector(".input").focus();
+    }
+}
+
+function handleContainerFocus({ target }) {
+    const element = getElementByAttr("data-action", target);
+
+    if (element && element.attrValue === "edit") {
+        target.addEventListener("blur", editPlaylistTitle);
+        target.addEventListener("keyup", blurEntryInput);
+    }
 }
 
 export {
     createPlaylistEntry,
     enableSyncBtn,
-    disableSyncBtn
+    disableSyncBtn,
+    updatePlaylistStats
 };
