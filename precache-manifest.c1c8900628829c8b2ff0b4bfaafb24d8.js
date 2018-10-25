67d4a12e1d23ca5cf34665e38114cfc2346813c3
Travis CI <ikandrius@gmail.com>
2018-10-25 14:08:52 +0000

    Deploy to GitHub Pages

---
+++ precache-manifest.c1c8900628829c8b2ff0b4bfaafb24d8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "09c3aa3494520ceb52c5",
+    "url": "main.css"
+  },
+  {
+    "revision": "09c3aa3494520ceb52c5",
+    "url": "main.js"
+  },
+  {
+    "revision": "edaf6ea76136acf9e9bf",
+    "url": "vendor.js"
+  },
+  {
+    "revision": "84ef2b7ee2827418cf2d",
+    "url": "2.js"
+  },
+  {
+    "revision": "a9de53f6e454d9f10421",
+    "url": "3.js"
+  },
+  {
+    "revision": "9668da2e724f68d3cc86",
+    "url": "4.js"
+  },
+  {
+    "revision": "7f3ebfd6890cb8171730",
+    "url": "5.js"
+  },
+  {
+    "revision": "04cc090ad41d6a8fba3b2103b2fb92f3",
+    "url": "index.html"
+  },
+  {
+    "revision": "7d6f38ea9b43b77f8188645cab4675a6",
+    "url": "assets/images/album-art-placeholder.png"
+  },
+  {
+    "revision": "23886e72342b42f3c92719206e7e7d48",
+    "url": "assets/images/main-icon.png"
+  },
+  {
+    "revision": "fc65dbc458dd82c0c345ab8e03ea1bfc",
+    "url": "assets/images/ring-alt.svg"
+  },
+  {
+    "revision": "5c5f115c32422bd733e2fbd3eccf44bd",
+    "url": "libs/dexie.min.js"
+  },
+  {
+    "revision": "1a5d3ed78092e9dde47411f37b0796c0",
+    "url": "ww.js"
+  },
+  {
+    "revision": "61b0c2579ba604635dc9ba443f79d913",
+    "url": "favicon.ico"
+  }
+];
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.859761436c839bc5ef89506e8635d4da.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("precache-manifest.c1c8900628829c8b2ff0b4bfaafb24d8.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 /* global workbox */
 
