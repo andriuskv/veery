4b33fc6c2b96723128d39a3d24147d38601e456e
Andrius Kvedaras <ikandrius@gmail.com>
2018-11-12 16:51:36 +0200

    Rewrite player message component

--- src/js/player/player.view.js
+++ src/js/player/player.view.js
@@ -1,36 +1,54 @@
-import { togglePanel } from "../panels.js";
-import { dispatchCustomEvent } from "../utils.js";
+import { dispatchCustomEvent, removeElement } from "../utils.js";
 
-function getPlayerMessageCb(title, body = "") {
-    return (id, { element }) => {
-        element.insertAdjacentHTML("beforeend", `
-            <div id="${id}" class="panel player-message">
-                <h3 class="panel-title">${title}</h3>
-                ${body && `<p class="player-message-body">${body}</p>`}
-            </div>
-        `);
-    };
+function createMessageContainer() {
+    const div = document.createElement("div");
+
+    div.id = "js-player-messages";
+    div.classList.add("player-messages");
+    document.getElementById("js-player").appendChild(div);
+
+    return div;
 }
 
-function getServiceWorkerMessageCb() {
-    return (id, { element }) => {
-        element.insertAdjacentHTML("beforeend", `
-            <div id="${id}" class="panel player-message service-worker-message">
-                <p class="player-message-body">Update is available, please refresh.</p>
-                <button id="js-player-message-btn" class="btn-icon player-message-btn">Refresh</button>
-            </div>
-        `);
-        document.getElementById("js-player-message-btn").addEventListener("click", refreshPage, { once: true });
-    };
+function getMessageContainer() {
+    return document.getElementById("js-player-messages") || createMessageContainer();
 }
 
-function showPlayerMessage({ title, body, callback = getPlayerMessageCb }) {
-    const createMessage = callback(title, body);
+function removePlayerMessage() {
+    const element = getMessageContainer();
 
-    togglePanel("js-player-message", createMessage, {
-        element: document.getElementById("js-player"),
-        removeOnClick: true
-    });
+    removeElement(element.lastElementChild);
+
+    if (!element.childElementCount) {
+        removeElement(element);
+    }
+}
+
+function showPlayerMessage({ title, body }) {
+    const element = getMessageContainer();
+
+    element.insertAdjacentHTML("afterBegin", `
+        <div class="player-message">
+            <h3 class="player-message-title">${title}</h3>
+            <p class="player-message-body">${body}</p>
+        </div>
+    `);
+
+    setTimeout(removePlayerMessage, 6000);
+}
+
+function showServiceWorkerMessage() {
+    const element = getMessageContainer();
+
+    element.insertAdjacentHTML("afterBegin", `
+        <div class="player-message service-worker-message">
+            <p class="player-message-body">Update is available, please refresh.</p>
+            <button id="js-player-message-btn" class="btn-icon player-message-btn">Refresh</button>
+        </div>
+    `);
+    document.getElementById("js-player-message-btn").addEventListener("click", refreshPage);
+
+    setTimeout(removePlayerMessage, 6000);
 }
 
 function refreshPage() {
@@ -43,15 +61,14 @@ function emitConnectivityStatus() {
 
 window.addEventListener("sw-state-change", ({ detail }) => {
     if (detail === "update") {
-        showPlayerMessage({
-            callback: getServiceWorkerMessageCb
-        });
+        showServiceWorkerMessage();
     }
 });
 
 window.addEventListener("online", emitConnectivityStatus);
 window.addEventListener("offline", emitConnectivityStatus);
 window.addEventListener("load", emitConnectivityStatus);
+
 export {
     showPlayerMessage
 };
