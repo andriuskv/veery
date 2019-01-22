d5ee74e1bc3623f114d3ea0aa0876897283bdcca
Andrius Kvedaras <ikandrius@gmail.com>
2019-01-22 17:42:26 +0200

    Deploy to GitHub Pages

---
+++ precache-manifest.731d669f428b15a5453eea9c7ed14e19.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d3b40e07510f9d82c0f",
+    "url": "main.css"
+  },
+  {
+    "revision": "4d3b40e07510f9d82c0f",
+    "url": "main.js"
+  },
+  {
+    "revision": "af57b6ec755690c0a39d",
+    "url": "vendor.js"
+  },
+  {
+    "revision": "4a5bf95dc1e6c2d4bcf9",
+    "url": "2.js"
+  },
+  {
+    "revision": "64e0862068e012edca1c",
+    "url": "3.js"
+  },
+  {
+    "revision": "dd5fafc70578e067c92a",
+    "url": "4.js"
+  },
+  {
+    "revision": "155f535d54c4ec3bc9c0",
+    "url": "5.js"
+  },
+  {
+    "revision": "e78f7bd5fede7a2cfb7a27df3a700c42",
+    "url": "index.html"
+  }
+];
---
+++ sw.js
@@ -0,0 +1,25 @@
+importScripts("precache-manifest.731d669f428b15a5453eea9c7ed14e19.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+/* global workbox */
+
+workbox.setConfig({
+    debug: false
+});
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
+self.addEventListener("install", event => {
+    event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+            .catch(() => caches.match("index.html"))
+    );
+});
+
