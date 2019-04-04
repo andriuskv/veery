4d591e07396297a256c2cec1dd16895f23a0d1de
Travis CI <ikandrius@gmail.com>
2019-04-04 09:29:14 +0000

    Deploy to GitHub Pages

---
+++ precache-manifest.2d4c22764a40a9ee6da460379ff07907.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c4de475997adfd018e027d6484cb5b9",
+    "url": "assets/images/album-art-placeholder.png"
+  },
+  {
+    "revision": "c5a2d3d8ee0dcfeaa2ed00be845eba4c",
+    "url": "assets/images/main-icon.png"
+  },
+  {
+    "revision": "92b347bfba388b957b82c13a6ac88f59",
+    "url": "assets/images/ring-alt.svg"
+  },
+  {
+    "revision": "36fa34850e332e4c67d8e0488ff09027",
+    "url": "favicon.png"
+  },
+  {
+    "revision": "c7d5c848cf775c107881f867406dea78",
+    "url": "index.html"
+  },
+  {
+    "revision": "5c5f115c32422bd733e2fbd3eccf44bd",
+    "url": "libs/dexie.min.js"
+  },
+  {
+    "revision": "57fd8780bffe695bb520",
+    "url": "main.css"
+  },
+  {
+    "revision": "57fd8780bffe695bb520",
+    "url": "main.js"
+  },
+  {
+    "revision": "c1ab8192840629951c63",
+    "url": "vendor.js"
+  },
+  {
+    "revision": "9993cf18acf2a47790d90f71dc9bf1ef",
+    "url": "ww.js"
+  }
+]);
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.7da8f6f2874ec7e3676252b1bbbcbe49.js", "https://storage.googleapis.com/workbox-cdn/releases/4.1.1/workbox-sw.js");
+importScripts("precache-manifest.2d4c22764a40a9ee6da460379ff07907.js", "https://storage.googleapis.com/workbox-cdn/releases/4.2.0/workbox-sw.js");
 
 /* global workbox */
 
@@ -19,7 +19,6 @@ self.addEventListener("fetch", event => {
     event.respondWith(
         caches.match(event.request)
             .then(response => response || fetch(event.request))
-            .catch(() => caches.match("index.html"))
     );
 });
 
